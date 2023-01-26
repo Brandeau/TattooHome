81980f8401cb741e5a6e2cdd093a6543329c7415
Brandeau <diego.andrade.y@gmail.com>
2023-01-26 11:15:35 -0300

    features

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-const rightArrow = document.getElementById("right-arrow");
+const leftArrowWrapper = document.getElementById("arrow-wrapper-left");
+const rightArrowWrapper = document.getElementById("arrow-wrapper-right");
+const gallery = document.getElementById("tmblr-full");
+const numOfImages = document.querySelectorAll('.gallery-images').length;
 
-function pressRightArrow(){
+function scrollToTheRight(){
 
-    rightArrow.addEventListener('keydown', (event) => )
+    if(gallery.scrollLeft === gallery.offsetWidth * (numOfImages - 1)) {gallery.scrollBy({
+        left: -(gallery.offsetWidth * (numOfImages - 1)), 
+        top: 0,
+        behavior: 'smooth',
+    
+    })}else{
+    
+        gallery.scrollBy({
+        left: gallery.offsetWidth / (numOfImages - 1), 
+        top: 0,
+        behavior: 'smooth',
+    
+    })}
+
 }
+
+function scrollToTheLeft(){
+
+    if(gallery.scrollLeft === 0) {gallery.scrollBy({
+        left: (gallery.offsetWidth * (numOfImages - 1)), 
+        top: 0,
+        behavior: 'smooth',
+    
+    })}else{
+    
+        gallery.scrollBy({
+        left: -(gallery.offsetWidth / (numOfImages - 1)), 
+        top: 0,
+        behavior: 'smooth',
+    
+    })}
+
+    console.log(gallery.scrollTop);
+
+}
+
+leftArrowWrapper.addEventListener("click", scrollToTheLeft);
+
+rightArrowWrapper.addEventListener("click", scrollToTheRight);
+
