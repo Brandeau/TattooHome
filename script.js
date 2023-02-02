92c6f971c0d220dd4462eaa495c073b5e92cddb0
Brandeau <diego.andrade.y@gmail.com>
2023-02-01 22:12:45 -0300

    features: change styles and add code for tumblr

--- script.js
+++ script.js
@@ -4,35 +4,23 @@ const gallery = document.getElementById("carrousel");
 const numOfImages = document.querySelectorAll(".gallery-images").length;
 
 function scrollToTheRight() {
-  if (gallery.scrollLeft === gallery.offsetWidth * (numOfImages - 1)) {
-    gallery.scrollBy({
-      left: -(gallery.offsetWidth * (numOfImages - 1)),
-      top: 0,
-      behavior: "smooth",
-    });
-  } else {
+
     gallery.scrollBy({
       left: gallery.offsetWidth / (numOfImages - 1),
       top: 0,
       behavior: "smooth",
     });
-  }
+
 }
 
 function scrollToTheLeft() {
-  if (gallery.scrollLeft === 0) {
-    gallery.scrollBy({
-      left: (gallery.offsetWidth * (numOfImages - 1)),
-      top: 0,
-      behavior: "smooth",
-    });
-  } else {
+ 
     gallery.scrollBy({
       left: -(gallery.offsetWidth / (numOfImages - 1)),
       top: 0,
       behavior: "smooth",
     });
-  }
+  
 
   console.log(gallery.scrollTop);
 }
@@ -40,3 +28,5 @@ function scrollToTheLeft() {
 leftArrowWrapper.addEventListener("click", scrollToTheLeft);
 
 rightArrowWrapper.addEventListener("click", scrollToTheRight);
+
+ 
